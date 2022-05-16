3febcdd5487c3ebd04ab2ab5dadcb47152b022e6
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-17 01:23:16 +0300

    defined components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,31 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { CheckOutComponent } from './Components/check-out/check-out.component';
+import { OrderSuccessComponent } from './Components/order-success/order-success.component';
+import { TrackOrdersComponent } from './Components/track-orders/track-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    TrackOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
