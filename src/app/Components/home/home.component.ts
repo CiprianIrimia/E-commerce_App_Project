03d40870ec7cebb563e5a557722876e678ddd32c
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-19 03:03:46 +0300

    translated Homepage

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  subtitle: string = 'BORN FROM THE PURITY OF NATURE';
+  redirectAboutUs: string = 'About Us';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/Components/home/homepage-carousel/homepage-carousel.component.ts
+++ src/app/Components/home/homepage-carousel/homepage-carousel.component.ts
@@ -5,6 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './homepage-carousel.component.html',
 })
 export class HomepageCarouselComponent {
+  image1H3: string = 'WE SEE THE LIGHT OF THE DAY';
+  image1P: string = 'Our wheat ears are born from the fertile land';
+  image2H3: string = 'WE LOOK AT THE SUN';
+  image2P: string = 'They flatter themselves in the wind and caress the sun';
+  image3H3: string = 'THE GREAT MOMENT HAS ARRIVED!';
+  image3P: string = `Follow the long road to the skilled hands of our bakers. Let's get to know each other...`;
+
   images = [107, 337, 165].map((n) => `https://picsum.photos/id/${n}/1600/600`);
 
   constructor() {}
