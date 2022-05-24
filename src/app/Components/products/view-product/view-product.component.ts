3a48ea8e9f0e53ed07da32e50219e48575533aac
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-24 22:17:18 +0300

    getting productId in view-product

--- src/app/Components/products/view-product/view-product.component.ts
+++ src/app/Components/products/view-product/view-product.component.ts
@@ -1,15 +1,22 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { prodInterface } from 'src/app/models/prodInterface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
-  styleUrls: ['./view-product.component.css']
+  styleUrls: ['./view-product.component.css'],
 })
 export class ViewProductComponent implements OnInit {
+  public productId: string | null = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param) => {
+      this.productId = param.get('productId');
+    });
   }
-
 }
