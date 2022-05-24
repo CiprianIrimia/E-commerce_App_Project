3f197acf82b6ab17f0cade1a182fc54e0bf32bad
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-24 14:49:37 +0300

    added getAllProducts service and handleError function

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { prodInterface } from '../models/prodInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private serverUrl: string = `http://localhost:5000/`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public getAllProducts(): Observable<prodInterface[]> {
+    let dataUrl: string = `${this.serverUrl}/products`;
+    return this.httpClient.get<prodInterface[]>(dataUrl).pipe();
+  }
+
+  public handleError(error: HttpErrorResponse) {
+    let errorMessage: string = '';
+    if (error.error instanceof ErrorEvent) {
+      //client error
+      errorMessage = `Error : ${error.error.message}`;
+    } else {
+      //server error {
+      errorMessage = `Status : ${error.status} \n Message: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
