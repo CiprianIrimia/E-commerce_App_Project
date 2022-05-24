91a06ed0923672f763cb95bc9b714350172e593f
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-24 22:38:56 +0300

    subscribe for getting product object (by id)

--- src/app/Components/products/view-product/view-product.component.ts
+++ src/app/Components/products/view-product/view-product.component.ts
@@ -1,4 +1,4 @@
-import { ThisReceiver } from '@angular/compiler';
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { prodInterface } from 'src/app/models/prodInterface';
@@ -10,13 +10,32 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./view-product.component.css'],
 })
 export class ViewProductComponent implements OnInit {
+  public loading: boolean = false;
   public productId: string | null = null;
+  public product: prodInterface = {} as prodInterface;
+  public errorMessage: string | null = null;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((param) => {
       this.productId = param.get('productId');
     });
+    if (this.productId) {
+      this.loading = true;
+      this.productService
+        .getProduct(this.productId)
+        .subscribe((data: prodInterface) => {
+          this.product = data;
+          this.loading = false;
+        });
+    }
+  }
+
+  public isNotEmpty() {
+    return Object.keys(this.product).length > 0;
   }
 }
