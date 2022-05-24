cc9e1424cacc30fbba118eab5b73385701aa8851
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-05-24 16:01:49 +0300

    declare variables and inject productService

--- src/app/Components/products/product-manager/product-manager.component.ts
+++ src/app/Components/products/product-manager/product-manager.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { prodCategory } from 'src/app/models/prodCategory';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-manager',
   templateUrl: './product-manager.component.html',
-  styleUrls: ['./product-manager.component.css']
+  styleUrls: ['./product-manager.component.css'],
 })
 export class ProductManagerComponent implements OnInit {
+  public loading: boolean = false;
+  public products: prodCategory[] = [];
+  public errorMessage: string | null = null;
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
