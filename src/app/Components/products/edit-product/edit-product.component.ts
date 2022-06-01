e394ec38c37961a5d654e566b6d6ea07f711f76f
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-06-01 14:12:37 +0300

    defined edit-product propreties

--- src/app/Components/products/edit-product/edit-product.component.ts
+++ src/app/Components/products/edit-product/edit-product.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { prodCategory } from 'src/app/models/prodCategory';
+import { prodInterface } from 'src/app/models/prodInterface';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrls: ['./edit-product.component.css']
+  styleUrls: ['./edit-product.component.css'],
 })
 export class EditProductComponent implements OnInit {
+  public loading: boolean = false;
+  public productId: string | null = null;
+  public product: prodInterface = {} as prodInterface;
+  public errorMessage: string | null = null;
+  public categories: prodCategory[] = [] as prodCategory[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
