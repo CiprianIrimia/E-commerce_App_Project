86523cb0fad3d025422cfa7452215b3648836c00
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-06-03 21:49:52 +0300

    created login method

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
+  public loading: boolean = false;
+  public errorMessage: string | null = null;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  login() {
+    this.http.get<any>('http://localhost:5000/signupUsers').subscribe((res) => {
+      const user = res.find((a: any) => {
+        return (
+          a.email === this.loginForm.value.email &&
+          a.password === this.loginForm.value.password
+        );
+      });
+      if (user) {
+        alert('Login success');
+        this.loginForm.reset();
+        this.router.navigate(['products']);
+      } else {
+        alert('User not found!');
+      }
+      () => {
+        this.errorMessage = 'Something went wrong!';
+        this.loading = false;
+      };
+    });
+  }
 }
