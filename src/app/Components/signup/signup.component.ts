2aef44e871c95cec11d0f4e3c2cc3aeef2fefdc3
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-06-03 20:28:08 +0300

    created signUp method

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  public signupForm!: FormGroup;
+  public loading: boolean = false;
+  public errorMessage: string | null = null;
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullName: [''],
+      mobile: [''],
+      email: [''],
+      password: [''],
+    });
+  }
+
+  signUp() {
+    this.loading = true;
+    this.http
+      .post<any>('http://localhost:5000/signupUsers', this.signupForm.value)
+      .subscribe(
+        (res) => {
+          alert('Signup successfull');
+          this.signupForm.reset();
+          this.router.navigate(['login']);
+          this.loading = false;
+        },
+        (error: string) => {
+          this.errorMessage = error;
+          this.loading = false;
+        }
+      );
+  }
 }
