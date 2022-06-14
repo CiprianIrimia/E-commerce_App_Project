1b9a8bcf09fe62a2b2f433d00af465ab492ec7d4
CiprianIrimia <ciprian.irimia.is@gmail.com>
2022-06-14 23:01:21 +0300

    defined add, get & clearCart methods in cart.service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { prodInterface } from '../models/prodInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  items: prodInterface[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  addToCart(product: prodInterface) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
